de667ad387454e42c0cedc16916092364283b11d
sapeol <sudeep.kuchara@protonmail.com>
2022-12-17 00:56:59 +0530

    init

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,12 +1,17 @@
 from test_framework import generic_test
 
+# NOTE: First initialize the bits to zero then while x is not 0, use the formula
+# x &= x - 1 to flip the 1 bit to 0 and increase the bits variable by 1, return the bits
+# time complexity O(k) where is the number of 1 bits in a number
+
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    bits = 0
+    while x:
+        x &= x - 1
+        bits += 1
+    return bits
 
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("count_bits.py", "count_bits.tsv", count_bits))
